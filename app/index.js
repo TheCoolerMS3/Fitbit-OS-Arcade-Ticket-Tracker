61c0a3d37489ffeaa5b457fefd05e38d79e5283f
Eve <62806964+TheCoolerMS3@users.noreply.github.com>
2024-08-12 16:44:11 +0100

    Implement filesystem to have variable save imbetween sessions

--- app/index.js
+++ app/index.js
@@ -1,21 +1,44 @@
 import * as document from "document";
+import * as fs from "fs";
+import { me } from "appbit";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const ticketstext = document.getElementById("ticketstext");
 let tickets = 0;
 
+// If tickets file exists, set tickets variable to it, else set tickets variable to 0
+if (fs.existsSync("tickets.txt")) {
+    console.log("file exists!");
+    let fileContent = fs.readFileSync("tickets.txt", "ascii").trim(); 
+    console.log(fileContent);
+
+    tickets = parseInt(fileContent, 10);
+
+    // Failsafe in case there's something wrong
+    if (isNaN(tickets)) {
+        tickets = 0;  
+    }
+
+    ticketstext.text = tickets.toString(); 
+} else {
+    tickets = 0;
+    ticketstext.text = tickets.toString(); 
+}
+
+// Listen out for button presses to increment/decrement tickets variable
 add.addEventListener("click", (evt) => {
-    console.log(tickets)
-    tickets = (tickets + 1)
-    console.log (tickets)
-    ticketstext.text = (tickets)
-  })
-  
+    tickets += 1;
+    ticketstext.text = tickets.toString();  
+});
+
 minus.addEventListener("click", (evt) => {
-    console.log(tickets)
-    tickets = (tickets - 1)
-    console.log (tickets)
-    ticketstext.text = (tickets)
- })
-  
+    tickets -= 1;
+    ticketstext.text = tickets.toString();  
+});
+
+// Save tickets file on unload
+me.onunload = () => {
+    console.log('saving file');
+    fs.writeFileSync("tickets.txt", tickets.toString(), "ascii"); 
+};
