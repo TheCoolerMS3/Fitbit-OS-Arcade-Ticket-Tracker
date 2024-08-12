6963a1e07bfc851cb33283f19f9461179ebb64d9
Eve <62806964+TheCoolerMS3@users.noreply.github.com>
2024-08-12 15:50:24 +0100

    the one and only commit of 15 minutes left

--- app/index.js
+++ app/index.js
@@ -1,2 +1,21 @@
-let tickets = 0
-console.log("Hi")
+import * as document from "document";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const ticketstext = document.getElementById("ticketstext");
+let tickets = 0;
+
+add.addEventListener("click", (evt) => {
+    console.log(tickets)
+    tickets = (tickets + 1)
+    console.log (tickets)
+    ticketstext.text = (tickets)
+  })
+  
+minus.addEventListener("click", (evt) => {
+    console.log(tickets)
+    tickets = (tickets - 1)
+    console.log (tickets)
+    ticketstext.text = (tickets)
+ })
+  
